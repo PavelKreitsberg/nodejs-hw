98402dd40ab7399a0cff578d98c34c2194893408
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2023-01-05 22:21:44 +0200

    solve hw2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,55 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
 
-const listContacts = async () => {}
+const path = require('path');
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.join(__dirname, "./contacts.json")
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf8') 
+  return JSON.parse(data);
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath, 'utf8')
+  return JSON.parse(data).find(contact => contact.id === String(contactId)) 
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {  
+  const data = await fs.readFile(contactsPath, 'utf8')
+  fs.writeFile(contactsPath, JSON.stringify([...JSON.parse(data), body]))  
+}
+
+const removeContact = async (contactId) => {
+  const data = await fs.readFile(contactsPath, 'utf8')
+
+  const contactList = JSON.parse(data);
+
+  const index = contactList.findIndex(contact => contact.id === String(contactId));
+  if (index === -1) {
+    return null;
+  }
+  const deletedContact = contactList[index]
+  contactList.splice(index, 1)
+  fs.writeFile(contactsPath, JSON.stringify(contactList))
+  return deletedContact;
+}
+
+
+
+const updateContact = async (contactId, body) => {
+  const data = await fs.readFile(contactsPath, 'utf8');
+
+  const contactList = JSON.parse(data)
+
+  const index = contactList.findIndex(contact => contact.id === String(contactId));
+  if (index === -1) {
+    return null;
+  }
+  contactList[index] = {id: contactId, ...body}
+
+  fs.writeFile(contactsPath, JSON.stringify(contactList))
+  return contactList[index];
+}
 
 module.exports = {
   listContacts,
