5961849bee78dc4753ac5ccfd317e61700e5ac10
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2023-01-28 13:48:36 +0200

    solwe hw-5

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const login = require("./login");
 const signup = require("./signup");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   signup,
   login,
   getCurrent,
   logout,
+  updateAvatar,
 };
--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,6 +1,8 @@
 const { Conflict } = require("http-errors");
 const bcrypt = require("bcryptjs");
 
+const gravatar = require("gravatar");
+
 const { User } = require("../../models");
 
 const signup = async (req, res, next) => {
@@ -12,12 +14,14 @@ const signup = async (req, res, next) => {
   }
 
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const avatarUrl = gravatar.url(email);
 
   const result = await User.create({
     password: hashPassword,
     email,
     subscription,
     token,
+    avatarUrl,
   });
   res.status(201).json({
     status: "success",
@@ -25,6 +29,7 @@ const signup = async (req, res, next) => {
     user: {
       email,
       subscription,
+      avatarUrl,
     },
   });
 };
